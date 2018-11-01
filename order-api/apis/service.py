bf0380ac0f2c7ed3831498198d51376793680698
Nam Nguyen <nnguyen2@deloitte.com.au>
2018-11-01 14:51:38 +1100

    Update part1

--- order-api/apis/service.py
+++ order-api/apis/service.py
@@ -1,21 +1,15 @@
 import uuid
 import logging
-import json
-from kafka import KafkaProducer
 
 log = logging.getLogger(__name__)
 
 class OrderService(object):
 
     def __init__(self):
-        self.__topic = "order-service-topic"
-        self.__producer = KafkaProducer(
-            bootstrap_servers='kafka:9092',
-            value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+        pass
 
     def create(self, data):
         data['id'] = str(uuid.uuid4())
-        self.__producer.send(self.__topic, data)
         return data['id']
 
 _srv = OrderService()
